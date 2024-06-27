d6bca21c0cb72ecbe66e355aac7400c32fdc8f7e
Yoseph Gebeyehu <yosephgebeyehu73@gmail.com>
2024-06-27 15:03:43 +0300

    Update the lorem ipsum texts with real texts.Update some media query styles.

--- portfolio/src/Home/AboutMe.jsx
+++ portfolio/src/Home/AboutMe.jsx
@@ -3,22 +3,41 @@ export default function AboutMe() {
     <section className="about--me--section" id="aboutMe">
       <div className="about--me--container">
         <div className="about--me--pic">
-          <img src="./img/home.png" alt="Placeholder" />
+          <img src="./img/home.png" alt="Yoseph's portrait" />
         </div>
         <div className="about--me--paragraph">
           <p className="about--me--title">About</p>
           <h2 className="about--me--heading">About Me</h2>
           <div className="about--me--description">
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
-              odit eius tempore. Cumque autem quidem exercitationem. Aut ab
-              neque corporis deleniti pariatur saepe eius inventore in, facere,
-              ratione, laudantium architecto? Lorem ipsum dolor sit amet
-              consectetur adipisicing elit. Inventore, blanditiis.
+              Hi, I'm Yoseph, a dedicated and passionate Software Engineer
+              specializing in both web and mobile application development. I
+              graduated as Computer Enginner from Addis Ababa Science and
+              Technology Universtiy. With a strong background in Flutter and
+              Dart, I have created numerous high-quality, cross-platform mobile
+              apps that provide excellent user experiences.
             </h3>
             <h3>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Dignissimos nemo perspiciatis quas natus veritatis!
+              I have been working at Chapa Financial Technologies for 18 months.
+              And I developed mobile applications. I have been gathering a lot
+              of skills at this wonderful company.
+            </h3>
+            <h3>
+              On the web front, I leverage the power of React.js to build
+              responsive and dynamic user interfaces. My expertise extends to
+              backend development with Firebase, ensuring that the apps I
+              develop are secure, scalable, and efficient.
+            </h3>
+            <h3>
+              I am continuously exploring new technologies and methodologies to
+              enhance my skills and stay updated with industry trends. Whether
+              it's developing innovative solutions or optimizing existing ones,
+              I am committed to delivering exceptional results.
+            </h3>
+            <h3>
+              Outside of coding, I enjoy contributing to open-source projects
+              and engaging with the tech community to share knowledge and learn
+              from others.
             </h3>
           </div>
         </div>
--- portfolio/src/Home/HomePage.jsx
+++ portfolio/src/Home/HomePage.jsx
@@ -4,17 +4,21 @@ export default function HomePage() {
       <div className="home--container">
         <div className="home--contents">
           <p className="home--title">Hey I'm Yoseph</p>
-          <h2 className="home--heading">Full Stack Developer</h2>
+          <h2 className="home--heading">Software Engineer</h2>
           <p className="home--description">
-            Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+            I am a passionate Software Engineer specializing in modern web and
+            mobile application development. With extensive experience in Flutter
+            and Dart for crafting high-quality, cross-platform mobile apps, and
+            expertise in React.js for building responsive, user-friendly web
+            interfaces, I strive to deliver seamless user experiences.
             <br />
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure sunt
-            maxime nisi exercitationem laborum!
+            Leveraging Firebase for robust backend services, I ensure secure and
+            scalable solutions. Let's build something amazing together!
           </p>
           <button className="home--btn--primary">Get In Touch</button>
         </div>
         <div className="home--pic">
-          <img src="./img/home.png" alt="" />
+          <img src="./img/home.png" alt="Yoseph's portrait" />
         </div>
       </div>
     </section>
